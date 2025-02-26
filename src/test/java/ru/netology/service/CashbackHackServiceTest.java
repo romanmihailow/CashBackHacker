fa8d85b06f88f0294b381e8a82f822ca066ab104
romanmihailow <romanmihailow@gmail.com>
2025-02-26 09:25:21 +0500

    test 2,3

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,10 +5,26 @@ import org.testng.annotations.Test;
 
 public class CashbackHackServiceTest {
 
-    public void testCashbackHackService() {
+    @Test
+    public void testCashbackHackService900() {
         CashbackHackService service = new CashbackHackService();
         int result = service.remain(900);
         Assert.assertEquals(result, 100, "Ожидаемый остаток для 900: 100");
     }
+
+    @Test
+    public void testCashbackHackService1000() {
+        CashbackHackService service = new CashbackHackService();
+        int result = service.remain(1000);
+        Assert.assertEquals(result, 1000, "Ожидаемый остаток для 1000: 1000");
+    }
+    @Test
+    public void testCashbackHackService1100() {
+        CashbackHackService service = new CashbackHackService();
+        int result = service.remain(1100);
+        Assert.assertEquals(result, 900, "Ожидаемый остаток для 1100: 900");
+    }
+
+
 }
 
